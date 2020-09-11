eefec6775b1011808e00f65ededb38fbc58293cf
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-09-10 17:05:17 -0700

    create shop data, ShopPage, CollectionPreview components without styling

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
+      <Route exact path='/shop' component={ShopPage} />
     </Switch>
   );
 }
