ac34bddee4ee6c1c5eec815cbcd9f31140c69ed9
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-12-16 15:11:12 -0800

    add redux persist and start using persistor

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //gives us the localStorage object on the window browser.
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root', //tells at what point within reducer object we want to start storing everything
+  storage,
+  whitelist: ['cart'] //reducers I want to persist
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-});
+})
+
+export default persistReducer(persistConfig, rootReducer);
