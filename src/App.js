cbbf2a517375852e39937866e1cf1cddf6822638
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-12-15 17:52:11 -0800

    create checkout component, improve the cart dropdown ux

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header-component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header-component';
+
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
@@ -47,7 +50,14 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to ='/' />) : (<SignInAndSignUpPage />)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route exact
+            path='/signin' 
+            render={() =>
+            this.props.currentUser ? (
+              <Redirect to ='/' />
+            ) : (
+              <SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
